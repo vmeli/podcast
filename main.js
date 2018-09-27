ac2852ba90d9b52c445556d82a3dd7594d657aab
vcelim <vickensi.dlcp@gmail.com>
2018-09-27 01:22:46 -0500

    adding-script-play-pause-undo

--- main.js
+++ main.js
@@ -3,21 +3,49 @@ var seekbarOuter = document.querySelector(".seekbarControl .outer");
 var volumeControl = document.querySelector(".volumeControl .wrapper");
 var btnPlay = document.getElementById("play");
 var btnStop = document.getElementById("stop");
+var btnUndo = document.getElementById("undo");
+
 var length, interval, seekbarPercentage;
 
 var audio =document.getElementById('audio');
 length = audio.duration;
 
-btnPlay.addEventListener('click', demo);
-btnStop.addEventListener('click', demo1);
+btnPlay.addEventListener('click', playing);
+btnStop.addEventListener('click', paused);
+btnUndo.addEventListener('click', undo);
 
-function demo(e) {
-	//document.querySelector('#play i').classList.toggle('fa-stop');
-	audio.play();
+//audio.addEventListener('play', messagePlay);
+//audio.addEventListener('pause', messagePause);
+audio.addEventListener('ended', messageEnded);
+
+function messageEnded() {
+	console.log("publicar un ads");
 }
 
-function demo1(e) {
+function playing(e) {
+	console.log(e.target);
+	if(document.querySelector("#play i").classList.value.includes("fa-play")) {
+		audio.play();
+		document.querySelector("#play i").classList.value = "fa fa-pause fa-stack";
+	}else {
+		audio.pause();
+		document.querySelector("#play i").classList.value = "fa fa-play fa-stack";
+	}
+}
+
+function paused(e) {
+	if(document.querySelector("#play i").classList.value.includes("fa-pause")) {
+		document.querySelector("#play i").classList.value = "fa fa-play fa-stack";
+	}
 	audio.pause();
+}
+
+function undo(e) {
+	if(document.querySelector("#play i").classList.value.includes("fa-pause")) {
+		audio.play();
+	}
+	document.querySelector(".seekbarControl .inner").removeAttribute("style");
+	audio.currentTime = 0;
 }
 
 interval = setInterval(function(){
